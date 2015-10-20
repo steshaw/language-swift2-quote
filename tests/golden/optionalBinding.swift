85ade62134117959137960af59f9b99103ad1953
Steven Shaw <steven@steshaw.org>
2015-10-20 11:11:18 +1000

    Added broken test for optional binding if statement.

---
+++ tests/golden/optionalBinding.swift
@@ -0,0 +1,5 @@
+if let actualNumber = Int(possibleNumber) {
+  print("possibleNumber has an integer value of ", actualNumber)
+} else {
+  print("possibleNumber could not be converted to an integer")
+}
